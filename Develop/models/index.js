5aac9395132a1235c0d2bed132fe864f953d274b
IsaiasQB <isaias.qb@hotmail.com>
2022-05-29 01:45:29 -0400

    Added routes for the Product and Category models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag_product',
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tag_product',
+  foreignKey: 'tag_id'
+})
+
 
 module.exports = {
   Product,
